875344360d7c42a0eb9fc126d0f14646ba90a09e
Eric Canull <ericcanull1000@msn.com>
2018-04-11 23:24:10 -0500

    added proper GNU GPL license comment  which includes no warranty clause

--- src/main/java/sortingalgoritms/sorts/CHeapSorter.java
+++ src/main/java/sortingalgoritms/sorts/CHeapSorter.java
@@ -1,11 +1,16 @@
 /*
- * Permissions of this free software license are conditioned on making available
- * complete source code of licensed works and modifications under the same 
- * license or the GNU GPLv3. Copyright and license notices must be preserved.
- * Contributors provide an express grant of patent rights. However, a larger 
- * work using the licensed work through interfaces provided by the licensed 
- * work may be distributed under different terms and without source code 
- * for the larger work.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package sortingalgoritms.sorts;
 
@@ -15,7 +20,6 @@ import sortingalgoritms.util.IComparable;
  * Implementation of the Heap Sort algorithm.
  *
  * @author Eric Canull
- * @version 1.0
  */
 public final class CHeapSorter extends ASortOperator {
 
